7cfd536e5a370369326359ba293162e0912a0bb7
n3lson0604 <n3lsoncassoma@gmail.com>
2025-09-10 21:09:17 +0100

    Add files via upload

---
+++ Soma1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int A, B, C;
+    printf("Primeiro numero");
+    scanf("%d", &A);
+    printf("Segundo numero");
+    scanf("%d", &B);
+    C=A+B;
+    printf("%d", &C);
+}
